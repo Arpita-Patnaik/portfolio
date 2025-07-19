d258bc9c4f6ef4748a4fddcbac360624413ce58e
Arpita <arpitapatnaik1367@gmail.com>
2025-07-19 19:40:34 +0530

    dome

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { ghPages } from 'vite-plugin-gh-pages'; // FIX: use named import
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,12 +13,11 @@ export default defineConfig(({ mode }) => ({
   base: '/portfolio/',
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'production' && ghPages(), // inside plugins
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-}));
+  }}
+));
